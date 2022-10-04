1910b3980016f0a096d2b4bb0f80b05c9db86c94
Badisse <badisse.bouabdallahb@laposte.net>
2022-10-05 01:50:54 +0200

    feat: add post customer

--- api/customer.api.js
+++ api/customer.api.js
@@ -1,3 +1,19 @@
+import { Alert } from "react-native";
+
 export const fetchCustomers = () =>
     fetch('http://192.168.1.90:3000/customer')
         .then(res => res.json());
+
+export const postCustomer = (customer) =>
+    fetch('http://192.168.1.90:3000/customer', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(customer)
+    })
+        .then(response => {
+            response.json()
+                .then(data => {
+                    Alert.alert(`Customer ${data.name} created !`);
+                    console.log(data)
+                });
+        })
